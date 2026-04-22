6d50f251c55acfa79622b04da77280da5f914f54
ujwal-gouda <ujwaldevrajgouda@gmail.com>
2026-04-22 18:01:11 +0530

    Initial Commit - Portfolio

---
+++ index.js
@@ -0,0 +1,48 @@
+let yearBox = document.getElementById("year");
+yearBox.textContent = new Date().getFullYear()
+
+// let experience = document.getElementById('experiance').children
+// console.log(experience)
+// experience.forEach(element => {
+//     addEventListener(element, () => {
+//         console.log('hover')
+//     })
+// });
+
+
+// let btn;
+let projects = [...document.querySelectorAll('.project')]
+console.log(projects)
+
+projects.forEach((e) => {
+    let btn = document.createElement('button')
+    let img = document.createElement('img')
+    img.src = '/icons/open_in_new.svg'
+    img.alt = 'openNewTab'
+    btn.appendChild(img)
+
+    Object.assign(btn.style, {
+        padding: '0', 
+        margin: '0', 
+        border: '0', 
+        backgroundColor: 'transparent', 
+        position: 'absolute', 
+        right: '0px', 
+        // bottom: '0',
+        display: 'none'
+    })
+    // btn.style.bottom = '5%'
+
+    if (!getComputedStyle(e).position || getComputedStyle(e).position === 'static') {
+        e.style.position = 'relative'
+    }
+
+    e.insertAdjacentElement("beforeend", btn)
+
+    e.addEventListener('mouseenter', ()=> {
+        btn.style.removeProperty('display')
+    })
+    e.addEventListener('mouseleave', ()=> {
+        btn.style.display = 'none'
+    })
+})
