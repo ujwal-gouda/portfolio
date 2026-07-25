ad2286d4e0f91f0fc9eed8bf68bc35f0fb62e2b5
ujwal-gouda <ujwaldevrajgouda@gmail.com>
2026-07-25 20:59:38 +0530

    Add experience details

--- index.js
+++ index.js
@@ -1,48 +1,43 @@
 let yearBox = document.getElementById("year");
-yearBox.textContent = new Date().getFullYear()
+yearBox.textContent = new Date().getFullYear();
 
-// let experience = document.getElementById('experiance').children
-// console.log(experience)
-// experience.forEach(element => {
-//     addEventListener(element, () => {
-//         console.log('hover')
-//     })
-// });
+let items = [
+    ...document.querySelectorAll(".project"),
+    ...document.querySelectorAll(".experience")
+];
 
+items.forEach((item) => {
+    let btn = document.createElement("button");
+    let img = document.createElement("img");
 
-// let btn;
-let projects = [...document.querySelectorAll('.project')]
-console.log(projects)
+    img.src = "/icons/open_in_new.svg";
+    img.alt = "Open in new tab";
 
-projects.forEach((e) => {
-    let btn = document.createElement('button')
-    let img = document.createElement('img')
-    img.src = '/icons/open_in_new.svg'
-    img.alt = 'openNewTab'
-    btn.appendChild(img)
+    btn.appendChild(img);
 
     Object.assign(btn.style, {
-        padding: '0', 
-        margin: '0', 
-        border: '0', 
-        backgroundColor: 'transparent', 
-        position: 'absolute', 
-        right: '0px', 
-        // bottom: '0',
-        display: 'none'
-    })
-    // btn.style.bottom = '5%'
+        padding: "0",
+        margin: "0",
+        border: "0",
+        background: "transparent",
+        position: "absolute",
+        top: "100%",
+        right: "0px",
+        display: "none",
+        cursor: "pointer"
+    });
 
-    if (!getComputedStyle(e).position || getComputedStyle(e).position === 'static') {
-        e.style.position = 'relative'
+    if (getComputedStyle(item).position === "static") {
+        item.style.position = "relative";
     }
 
-    e.insertAdjacentElement("beforeend", btn)
+    item.appendChild(btn);
 
-    e.addEventListener('mouseenter', ()=> {
-        btn.style.removeProperty('display')
-    })
-    e.addEventListener('mouseleave', ()=> {
-        btn.style.display = 'none'
-    })
-})
+    item.addEventListener("mouseenter", () => {
+        btn.style.display = "block";
+    });
+
+    item.addEventListener("mouseleave", () => {
+        btn.style.display = "none";
+    });
+});
